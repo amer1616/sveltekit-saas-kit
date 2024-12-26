a1e4b74edbac64071dc696ac306feb1405218f98
Amer <amer1616@gmail.com>
2024-12-26 11:46:00 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+
+<script lang="ts">
+
+</script>
+<div class="flex flex-col items-center justify-center h-screen">
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+</div>
+
+
+
